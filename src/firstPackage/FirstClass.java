39c8d198acad0a07d33a14cf5568e1ae8e41e36b
Алексей <aleksej@192.168.1.2>
2022-12-23 16:49:52 +0300

    first commit

---
+++ src/firstPackage/FirstClass.java
@@ -0,0 +1,10 @@
+package firstPackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author aleksej
+ *
+ */
+module git {
+}
